dcc12cdc9004cd84aef111a7ce94f1490977bb64
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2019-03-07 18:38:41 +0600

    add interface solution

---
+++ MVC.INTERFACE/ITeamService.cs
@@ -0,0 +1,24 @@
+﻿using MVC.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.INTERFACE
+{
+    public interface ITeamService 
+    {
+        IList<TEAM> GetList(string rowFrom, string rowTo, string searchText, string user);
+        TEAM Get(string ID);
+        bool Save(TEAM OFFICE_NOTE);
+        bool Update(TEAM OFFICE_NOTE);
+        bool Delete(TEAM EMPLOYEE);
+        bool IsDuplicate(TEAM EMPLOYEE);
+        double GetTotalItems(string pageNumber, string pageSize, string searchText, string user);
+        DataTable GetEmployeeInfoByTeam(string note_id);
+
+
+    }
+}
